c5826fb374628a73fd98ef25eab46270188b66ce
PatrickLSteward <70727884+PatrickLSteward@users.noreply.github.com>
2021-03-02 14:44:00 -0500

    Update cunit_tests.c

--- cunit_tests.c
+++ cunit_tests.c
@@ -20,11 +20,11 @@ int main(){
 
     //declare suites 
     CU_pSuite suite1 = CU_add_suite("test_add", 0,0);
-    CU_pSuite suite2 = CU_subtract_suite("test_subtract", 0,0);
+    CU_pSuite suite2 = CU_add_suite("test_subtract", 0,0);
 
     //add tests 
     CU_add_test(suite1, "test_add", test_add);
-    CU_add_test(suite2, "test_subtract",0,0);
+    CU_add_test(suite2, "test_add", test_subtract);
 
     //set output mode, run tests, clean up 
     CU_basid_set_mode(CU_BRM_VERBOSE);
